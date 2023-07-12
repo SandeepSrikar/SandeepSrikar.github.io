dd8d9c745eb815ffe7a9747df5973cd3a6424b6d
SandeepSrikar <sandeepveerendra123@gmail.com>
2023-07-12 23:27:47 +0530

    Add files from moved folder

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+var swiper = new swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween:20,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    naviagation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 4,
+        },
+    },
+});
